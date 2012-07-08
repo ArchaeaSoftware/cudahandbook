17fb328c1388bab5730917dc573422135cb91362
Nicholas Wilt <nicholas@archaeasoftware.com>
2012-07-08 08:48:52 -0700

    Shmoo over more-sensible values

--- reduction/reductionTemplated.cu
+++ reduction/reductionTemplated.cu
@@ -59,7 +59,6 @@ forkPrint( FILE *out, const char *fmt, ... )
     va_end(arg);
 }
 
-FILE *g_fileBlocks;
 FILE *g_fileShmoo;
 
 //
@@ -97,7 +96,6 @@ struct SharedMemory
 
 typedef struct TimingResult_struct {
     double us;
-    int fastestBlocks;
     int fastestThreads;
 } TimingResult;
 
@@ -112,7 +110,7 @@ chEventBandwidth( cudaEvent_t start, cudaEvent_t stop, double cBytes )
 
 typedef void (*pfnReduction)(CReduction_Sumi *out, CReduction_Sumi *partialSums, const int *in, size_t N, int cBlocks, int cThreads);
 
-template<class ReductionType, class T>
+template<typename ReductionType, typename T>
 double
 TimedReduction( 
     ReductionType *answer, const T *deviceIn, size_t N, int cBlocks, int cThreads,
@@ -150,77 +148,50 @@ Error:
     return ret;
 }
 
-int g_ShmooBlockMin = 2;
-int g_ShmooBlockMax = 900;
-int g_ShmooBlockStep = 1;
+int g_cBlocks = 900;
+
 int g_ShmooThreadMin = 32;
 int g_ShmooThreadMax = 512;
 
-size_t g_nMin;
-size_t g_nMax;
-size_t g_nStep;
-
-template<class ReductionType, class T>
+template<typename ReductionType, typename T>
 void
 Shmoo( TimingResult *timingResult,
        T *deviceData, size_t cInts, const ReductionType& expectedSum, 
-       bool bPrint, 
        void (*pfnReduce)(ReductionType *out, ReductionType *partialSums, 
            const T *in, size_t N, int cBlocks, int cThreads) )
 {
     double minTime = 1e6;
     int fastestThreads = 0;
-    int fastestBlocks = 0;
-    if ( bPrint ) {
-        forkPrint( g_fileBlocks, "Blocks\t" );
-        for ( int cThreads  = g_ShmooThreadMin; 
-                  cThreads <= g_ShmooThreadMax; 
-                  cThreads *= 2 ) {
-            forkPrint( g_fileBlocks, "%d\t", cThreads );
-        }
-        forkPrint( g_fileBlocks, "minTC\tminTime\n" );        
+    for ( int cThreads  = g_ShmooThreadMin; 
+              cThreads <= g_ShmooThreadMax; 
+              cThreads *= 2 ) {
+        printf( "%d\t", cThreads );
     }
-    for ( int cBlocks  = g_ShmooBlockMin; 
-              cBlocks <= g_ShmooBlockMax; 
-              cBlocks += g_ShmooBlockStep ) {
-        int fastestThreadsThisBlock = 0;
-        double minTimeThisBlock = 1e6;
-        if ( bPrint ) {
-            forkPrint( g_fileBlocks, "%d\t", cBlocks );
+    printf( "minTC\tminTime\n" );        
+    for ( int  cThreads = g_ShmooThreadMin; 
+              cThreads <= g_ShmooThreadMax; 
+              cThreads *= 2 ) {
+        ReductionType sum;
+        double us = TimedReduction( &sum, deviceData, cInts, g_cBlocks, cThreads, pfnReduce );
+        if( sum != expectedSum ) {
+            printf( "Sum is wrong: 0x%08x should be 0x%08x\n", sum.sum, expectedSum.sum );
+            exit(1);
         }
-        for ( int  cThreads = g_ShmooThreadMin; 
-                  cThreads <= g_ShmooThreadMax; 
-                  cThreads *= 2 ) {
-            ReductionType sum;
-            double us = TimedReduction( &sum, deviceData, cInts, cBlocks, cThreads, pfnReduce );
-            if( sum != expectedSum ) {
-                printf( "Sum is wrong: 0x%08x should be 0x%08x\n", sum.sum, expectedSum.sum );
-                exit(1);
-            }
-            if ( us < minTimeThisBlock ) {
-                minTimeThisBlock = us;
-                fastestThreadsThisBlock = cThreads;
-            }
-            if ( us < minTime ) {
-                minTime = us;
-                fastestBlocks = cBlocks;
-                fastestThreads = cThreads;
-            }
-            if ( bPrint ) {
-                forkPrint( g_fileBlocks, "%.2f\t", us );
-                if ( cThreads == 512 ) {
-                    forkPrint( g_fileBlocks, "%d\t%.2f\n", fastestThreadsThisBlock, minTimeThisBlock );
-                }
-            }
-            
+        if ( us < minTime ) {
+            minTime = us;
+            fastestThreads = cThreads;
         }
+        printf( "%.2f\t", us );
+        if ( cThreads == g_ShmooThreadMax ) {
+            printf( "%d\t%.2f\n", fastestThreads, minTime );
+        }
+        
     }
-    timingResult->fastestBlocks = fastestBlocks;
     timingResult->fastestThreads = fastestThreads;
     timingResult->us = minTime;
 }
 
-template<class ReductionType, class T>
+template<typename ReductionType, typename T>
 double
 usPerInvocation( int cIterations, size_t N,
     void (*pfnReduction)( ReductionType *out, ReductionType *partial, 
@@ -248,7 +219,7 @@ Error:
     return ret;
 }
 
-template<class ReductionType, class T>
+template<typename ReductionType, typename T>
 void
 ReportOverhead()
 {
@@ -260,9 +231,9 @@ ReportOverhead()
     printf( "\tReduction4: %.2f\n", usPerInvocation<ReductionType, T>( 100000, N, Reduction4 ) );
 }
 
-template<class ReductionType, class T>
+template<typename ReductionType, typename T>
 bool
-ShmooReport( size_t nMin, size_t nMax, size_t nStep, bool bFloat, bool bReportBlockSizes )
+ShmooReport( size_t N, bool bFloat )
 {
     cudaError_t status;
     cudaDeviceProp props;
@@ -270,24 +241,22 @@ ShmooReport( size_t nMin, size_t nMax, size_t nStep, bool bFloat, bool bReportBl
     T *hostData = 0;
     T *deviceData = 0;
 
-    forkPrint( g_fileShmoo, "Shmoo: %d %ss (start at %d, step by %d)\n", (int) nMax,
-        (bFloat) ? "float" : "int", (int) nMin, (int) nStep );
-    hostData = (T *) malloc( nMax*sizeof(T) );
+    forkPrint( g_fileShmoo, "Shmoo: %d %ss\n", (int) N, 
+        (bFloat) ? "float" : "int" );
+    hostData = (T *) malloc( N*sizeof(T) );
     if ( ! hostData )
         goto Error;
     CUDART_CHECK( cudaSetDeviceFlags( cudaDeviceMapHost ) );
-    CUDART_CHECK( cudaMalloc( &deviceData, nMax*sizeof(T) ) );
+    CUDART_CHECK( cudaMalloc( &deviceData, N*sizeof(T) ) );
     CUDART_CHECK( cudaGetDeviceProperties( &props, 0 ) );
-    CUDART_CHECK( cudaMemcpy( deviceData, hostData, nMax*sizeof(T), cudaMemcpyHostToDevice ) );
+    CUDART_CHECK( cudaMemcpy( deviceData, hostData, N*sizeof(T), cudaMemcpyHostToDevice ) );
 
-    if ( ! bReportBlockSizes ) {
-        forkPrint( g_fileShmoo, "N\tBlocks(1)\tThreads(1)\tus(1)\t"
-                                   "Blocks(2)\tThreads(2)\tus(2)\t"
-                                   "Blocks(3)\tThreads(3)\tus(3)\t"
-                                   "Blocks(4)\tThreads(4)\tus(4)\n" );
-    }
+    forkPrint( g_fileShmoo, "N\tThreads(1)\tus(1)\t"
+                               "Threads(2)\tus(2)\t"
+                               "Threads(3)\tus(3)\t"
+                               "Threads(4)\tus(4)\n" );
 
-    for ( size_t count = nMin; count <= nMax; count += nStep ) {
+    {
         TimingResult result[4];
 
         void (*rgpfnReduction[4])(ReductionType *out, 
@@ -299,23 +268,16 @@ ShmooReport( size_t nMin, size_t nMax, size_t nStep, bool bFloat, bool bReportBl
               Reduction4<ReductionType, T> };
         ReductionType sum;
 
-        if ( ! bReportBlockSizes ) {
-            forkPrint( g_fileShmoo, "%d\t", count );
-        }
+        forkPrint( g_fileShmoo, "%d\t", N );
 
-        for ( size_t i = 0; i < count; i++ ) {
+        for ( size_t i = 0; i < N; i++ ) {
             sum += hostData[i];
         }
 
         for ( size_t i = 0; i < 4; i++ ) {
-            if ( bReportBlockSizes ) {
-                forkPrint( g_fileShmoo, "Formulation %d, %d: reporting for all block sizes\n", i, count );
-            }
-            Shmoo( &result[i], deviceData, count, sum, bReportBlockSizes, rgpfnReduction[i] );
-            if ( ! bReportBlockSizes ) {
-                forkPrint( g_fileShmoo, "%d\t%d\t%.2f\t", result[i].fastestBlocks, 
-                    result[i].fastestThreads, result[i].us );
-            }
+            Shmoo( &result[i], deviceData, N, sum, rgpfnReduction[i] );
+            forkPrint( g_fileShmoo, "%d\t%.2f\t",
+                result[i].fastestThreads, result[i].us );
             fflush(stdout);
         }
         forkPrint( g_fileShmoo, "\n" );
@@ -335,18 +297,13 @@ Usage()
     printf( "        --float - perform analysis on float-valued reductions\n" );
     printf( "        --squares - perform analysis on sum+sum of squares reductions (i.e. more computation)\n" );
     printf( "        --overhead - instead of performing shmoo over range of input sizes, report kernel launch overhead\n" );
-    printf( "        --nMax <nMax> - number of elements (will be multiplied by 1024) to run analysis on\n" );
-    printf( "        --nMin <nMin> - start shmoo at this number of elements (scaled by 1024)\n" );
-    printf( "        --nStep <nStep> - step number of elements by this amount (scaled by 1024) until it reaches nMax\n" );
+    printf( "        --size <N> - number of Melements (scaled by 1048576)\n" );
 
-    printf( "        --blockMax <nMax> - maximum number of blocks to run analysis on\n" );
-    printf( "        --blockMin <nMin> - minimum number of blocks to run analysis on\n" );
-    printf( "        --blockStep <nStep> - step number of blocks to run analysis on\n" );
+    printf( "        --blocks <N> - number of blocks to run analysis on\n" );
 
-    printf( "        --threadMax <nMax> - maximum number of threads to run analysis on\n" );
-    printf( "        --threadMin <nMin> - minimum number of threads to run analysis on\n" );
+    printf( "        --threadMax <count> - maximum number of threads to run analysis on\n" );
+    printf( "        --threadMin <count> - minimum number of threads to run analysis on\n" );
 
-    printf( "        --blocksFile <filename> - name of file to which to dump blocks reporting\n" );
     printf( "        --shmooFile <filename> - name of file to which to dump shmoo reporting\n" );
 
 }
@@ -355,12 +312,11 @@ int
 main( int argc, char *argv[] )
 {
     int device = 0;
-    int nMini, nMaxi, nStepi;
-    size_t nMin, nMax, nStep;
+    int iN = 16;
+    size_t N;
     bool bDoFloats = false;
     bool bDoSquares = false;
     bool bDoOverhead = false;
-    bool bReportBlockSizes = false;
 
     if ( argc == 1 ) {
         Usage();
@@ -372,33 +328,13 @@ main( int argc, char *argv[] )
         cudaSetDevice( device );
     }
 
-    // By default, analyze just 1M elements, 256K at a time.
-    nMini = 256;
-    nStepi = 256;
-    nMaxi = 1024;
-    chCommandLineGet( &nMini, "nMin", argc, argv );
-    chCommandLineGet( &nStepi, "nStep", argc, argv );
-    chCommandLineGet( &nMaxi, "nMax", argc, argv );
-    if ( nMini > nMaxi ) {
-        fprintf( stderr, "nMax must be greater than or equal to nMin\n" );
-        exit(1);
-    }
-    chCommandLineGet( &g_ShmooBlockMin, "blockMin", argc, argv );
-    chCommandLineGet( &g_ShmooBlockMax, "blockMax", argc, argv );
-    if ( g_ShmooBlockMin > g_ShmooBlockMax ) {
-        fprintf( stderr, "blockMax must be greater than or equal to blockMin\n" );
-        exit(1);
-    }
-    chCommandLineGet( &g_ShmooBlockStep, "blockStep", argc, argv );
+    chCommandLineGet( &iN, "size", argc, argv );
+    chCommandLineGet( &g_cBlocks, "blocks", argc, argv );
     chCommandLineGet( &g_ShmooThreadMin, "threadMin", argc, argv );
     chCommandLineGet( &g_ShmooThreadMax, "threadMax", argc, argv );
     if ( g_ShmooThreadMin < 32 || g_ShmooThreadMax < 32 || 
          g_ShmooThreadMax > 512 || g_ShmooThreadMin > 512 ) {
         fprintf( stderr, "threadMin/threadMax must be in range 32..512\n" );
-        exit(1);
-    }
-    if ( g_ShmooBlockMin > g_ShmooBlockMax ) {
-        fprintf( stderr, "threadMax must be greater than or equal to threadMin\n" );
         exit(1);
     }
     if ( g_ShmooThreadMin > g_ShmooThreadMax ) {
@@ -410,23 +346,17 @@ main( int argc, char *argv[] )
         fprintf( stderr, "threadMin and threadMax must be powers of 2\n" );
         exit(1);
     }
-    nMin = (size_t) nMini * 1024;
-    nMax = (size_t) nMaxi * 1024;
-    nStep = (size_t) nStepi * 1024;
+    
+    N = (size_t) iN * 1048576;
     bDoFloats = chCommandLineGetBool( "float", argc, argv );
     bDoSquares = chCommandLineGetBool( "squares", argc, argv );
     bDoOverhead = chCommandLineGetBool( "overhead", argc, argv );
-    bReportBlockSizes = chCommandLineGetBool( "report-block-sizes", argc, argv );
 
     printf( "Threads: %d - %d\n", g_ShmooThreadMin, g_ShmooThreadMax );
-    printf( "Threads: %d - %d by %d\n", g_ShmooBlockMin, g_ShmooBlockMax, g_ShmooBlockStep );
+    printf( "Blocks: %d\n", g_cBlocks );
 
     {
         char *fname;
-        if ( chCommandLineGet( &fname, "blocksFile", argc, argv ) ) {
-            g_fileBlocks = fopen( fname, "wt" );
-            bReportBlockSizes = g_fileBlocks != NULL;
-        }
         if ( chCommandLineGet( &fname, "shmooFile", argc, argv ) ) {
             g_fileShmoo = fopen( fname, "wt" );
         }
@@ -444,24 +374,21 @@ main( int argc, char *argv[] )
 
     if ( bDoFloats ) {
         if ( bDoSquares ) {
-            ShmooReport<CReduction_Sumf_fsq, float>( nMin, nMax, nStep, true, bReportBlockSizes );
+            ShmooReport<CReduction_Sumf_fsq, float>( N, true );
         }
         else {
-            ShmooReport<CReduction_Sumf, float>( nMin, nMax, nStep, true, bReportBlockSizes );
+            ShmooReport<CReduction_Sumf, float>( N, true );
         }
     }
     else {
         if ( bDoSquares ) {
-            ShmooReport<CReduction_Sumi_isq, int>( nMin, nMax, nStep, false, bReportBlockSizes );
+            ShmooReport<CReduction_Sumi_isq, int>( N, false );
         }
         else {
-            ShmooReport<CReduction_Sumi, int>( nMin, nMax, nStep, false, bReportBlockSizes );
+            ShmooReport<CReduction_Sumi, int>( N, false );
         }
     }
     if ( g_fileShmoo ) {
         fclose( g_fileShmoo );
-    }
-    if ( g_fileBlocks ) {
-        fclose( g_fileBlocks );
     }
 }
