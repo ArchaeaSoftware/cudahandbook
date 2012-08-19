01915c388e421fa16d4e4b77c91ef3921d901f70
Nicholas Wilt <nicholas@archaeasoftware.com>
2012-08-19 12:13:07 -0700

    Atomic microdemos

---
+++ atomics/atomic32.cu
@@ -0,0 +1,54 @@
+/*
+ *
+ * atomic32.cu
+ *
+ * Microdemo for atomic operations on 32-bit integers in global memory.
+ *
+ * NOT INTENDED AS A SAMPLE FOR ANYTHING OTHER THAN CODE GENERATION.
+ *
+ * Build with: 
+ *     nvcc --gpu-architecture sm_xx --cubin atomic32.cu
+ *     cuobjdump --dump-sass atomic32.cubin
+ * (fill in xx with 11, 20, or 30 to examine code generation of Tesla,
+ * Fermi and Kepler, respectively)
+ *
+ * Copyright (c) 2011-2012, Archaea Software, LLC.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions 
+ * are met: 
+ *
+ * 1. Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in 
+ *    the documentation and/or other materials provided with the 
+ *    distribution. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+__global__ void
+AddTo32( int *sum, int *out, const int *pIn )
+{
+    (void) atomicAdd( &out[threadIdx.x], *pIn );
+}
+
+__global__ void
+Return32( int *sum, int *out, const int *pIn )
+{
+    out[threadIdx.x] = atomicAdd( &sum[threadIdx.x], *pIn );
+}
---
+++ atomics/atomic64Shared.cu
@@ -0,0 +1,64 @@
+/*
+ *
+ * atomic32Shared.cu
+ *
+ * Microdemo for atomic 32-bit integers in shared memory.
+ *
+ * NOT INTENDED AS A SAMPLE FOR ANYTHING OTHER THAN CODE GENERATION.
+ *
+ * Build with: 
+ *     nvcc --gpu-architecture sm_xx --cubin atomic64Shared.cu
+ *     cuobjdump --dump-sass atomic64Shared.cubin
+ * (fill in xx with 12, 20, or 30 to examine code generation of Tesla,
+ * Fermi and Kepler, respectively)
+ *
+ * Copyright (c) 2011-2012, Archaea Software, LLC.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions 
+ * are met: 
+ *
+ * 1. Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in 
+ *    the documentation and/or other materials provided with the 
+ *    distribution. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+__global__ void
+AddTo64( long long *sum, long long *out, const long long *pIn )
+{
+    extern __shared__ long long s[];
+    s[threadIdx.x] = pIn[threadIdx.x];
+    __syncthreads();
+    (void) atomicAdd( &s[threadIdx.x], *pIn );
+    __syncthreads();
+    out[threadIdx.x] = s[threadIdx.x];
+}
+
+__global__ void
+Return64( long long *sum, long long *out, const long long *pIn )
+{
+    extern __shared__ long long s[];
+    s[threadIdx.x] = pIn[threadIdx.x];
+    __syncthreads();
+    (void) atomicAdd( &s[threadIdx.x], *pIn );
+    __syncthreads();
+    out[threadIdx.x] = s[threadIdx.x];
+}
