99e7fa37a7fc2240623bc89f9d6cab5508ec280b
Nicholas Wilt <nicholas@archaeasoftware.com>
2012-08-19 12:13:07 -0700

    Atomic microdemos

---
+++ atomics/README.txt
@@ -0,0 +1,23 @@
+This directory contains sample code with no purpose other than to
+illustrate the code generated for atomic operations on global and
+shared memory.  
+
+To see the generated machine code, compile with:
+
+nvcc --cubin --gpu-architecture sm_xx <filename>
+
+then disassemble the compiled microcode with cudumpobj:
+
+cudumpobj --dump-sass <filename>.cubin
+
+
+Memory     Operand              File
+
+Global     int                  atomic32.cu
+Global     unsigned long long   atomic64.cu
+Global     float                atomicFloat.cu
+
+Shared     int                  atomic32Shared.cu
+Shared     unsigned long long   atomic64Shared.cu
+Shared     float                atomicFloatShared.cu
+
---
+++ atomics/atomicFloatShared.cu
@@ -0,0 +1,62 @@
+/*
+ *
+ * atomicFloatShared.cu
+ *
+ * Microdemo for atomic floating point addition in shared memory.
+ *
+ * NOT INTENDED AS A SAMPLE FOR ANYTHING OTHER THAN CODE GENERATION.
+ *
+ * Build with: 
+ *     nvcc --gpu-architecture sm_20 --cubin atomicFloatShared.cu
+ *     cuobjdump --dump-sass atomicFloatShared.cubin
+ *
+ * Copyright (c) 2011-2012, Archaea Software, LLC.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions 
+ * are met: 
+ *
+ * 1. Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in 
+ *    the documentation and/or other materials provided with the 
+ *    distribution. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+__global__ void
+AddToFloat( float *sum, float *out, const float *pIn )
+{
+    extern __shared__ float s[];
+    s[threadIdx.x] = pIn[threadIdx.x];
+    __syncthreads();
+    (void) atomicAdd( &s[threadIdx.x], *pIn );
+    __syncthreads();
+    out[threadIdx.x] = s[threadIdx.x];
+}
+
+__global__ void
+ReturnFloat( float *sum, float *out, const float *pIn )
+{
+    extern __shared__ float s[];
+    s[threadIdx.x] = pIn[threadIdx.x];
+    __syncthreads();
+    (void) atomicAdd( &s[threadIdx.x], *pIn );
+    __syncthreads();
+    out[threadIdx.x] = s[threadIdx.x];
+}
