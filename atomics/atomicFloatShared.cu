305f17be4951098a279e839f34c0260a775e2357
Nicholas Wilt <nicholas@archaeasoftware.com>
2012-09-05 22:18:59 -0700

    Remove fire-and-forget formulations of shared atomics

--- atomics/atomicFloatShared.cu
+++ atomics/atomicFloatShared.cu
@@ -40,17 +40,6 @@
  */
 
 __global__ void
-AddToFloat( float *sum, float *out, const float *pIn )
-{
-    extern __shared__ float s[];
-    s[threadIdx.x] = pIn[threadIdx.x];
-    __syncthreads();
-    (void) atomicAdd( &s[threadIdx.x], *pIn );
-    __syncthreads();
-    out[threadIdx.x] = s[threadIdx.x];
-}
-
-__global__ void
 ReturnFloat( float *sum, float *out, const float *pIn )
 {
     extern __shared__ float s[];
