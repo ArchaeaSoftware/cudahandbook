47a19c5957709cd2196f6c79c79c24c8de90bb1e
Nicholas Wilt <nicholas@archaeasoftware.com>
2012-11-05 06:51:48 -0800

    Fix copyright and add 1024-thread case

--- reduction/reduction5Templated.cuh
+++ reduction/reduction5Templated.cuh
@@ -1,7 +1,38 @@
 /*
  *
- * Copyright (C) 2011 by Archaea Software, LLC.  
- *      All rights reserved.
+ * reduction5Templated.cuh
+ *
+ * CUDA header for templated formulation of reduction.
+ *
+ * Build with: nvcc -I ../chLib <options> reduction5Templated.cuh
+ * Requires: No minimum SM requirement.
+ *
+ * Copyright (c) 2011-2012, Archaea Software, LLC.
+ * All rights reserved.
+
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions 
+ * are met: 
+
+ * 1. Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in 
+ *    the documentation and/or other materials provided with the 
+ *    distribution. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
  *
  */
 
@@ -26,6 +57,12 @@ Reduction5_kernel( ReductionType *out, const T *in, size_t N )
     sPartials[tid] = sum;
     __syncthreads();
 
+    if (numThreads >= 1024) { 
+        if (tid < 512) {
+            sPartials[tid] += sPartials[tid + 512];
+        } 
+        __syncthreads();
+    }
     if (numThreads >= 512) { 
         if (tid < 256) {
             sPartials[tid] += sPartials[tid + 256];
@@ -75,15 +112,16 @@ Reduction5( ReductionType *out, ReductionType *partial, const T *in, size_t N, i
         numBlocks = (N+numThreads-1)/numThreads;
     }
     switch ( numThreads ) {
-        case   1: return Reduction5_template<ReductionType, T,  1>( out, partial, in, N, numBlocks );
-        case   2: return Reduction5_template<ReductionType, T,  2>( out, partial, in, N, numBlocks );
-        case   4: return Reduction5_template<ReductionType, T,  4>( out, partial, in, N, numBlocks );
-        case   8: return Reduction5_template<ReductionType, T,  8>( out, partial, in, N, numBlocks );
-        case  16: return Reduction5_template<ReductionType, T, 16>( out, partial, in, N, numBlocks );
-        case  32: return Reduction5_template<ReductionType, T, 32>( out, partial, in, N, numBlocks );
-        case  64: return Reduction5_template<ReductionType, T, 64>( out, partial, in, N, numBlocks );
-        case 128: return Reduction5_template<ReductionType, T,128>( out, partial, in, N, numBlocks );
-        case 256: return Reduction5_template<ReductionType, T,256>( out, partial, in, N, numBlocks );
-        case 512: return Reduction5_template<ReductionType, T,512>( out, partial, in, N, numBlocks );
+        case    1: return Reduction5_template<ReductionType, T,   1>( out, partial, in, N, numBlocks );
+        case    2: return Reduction5_template<ReductionType, T,   2>( out, partial, in, N, numBlocks );
+        case    4: return Reduction5_template<ReductionType, T,   4>( out, partial, in, N, numBlocks );
+        case    8: return Reduction5_template<ReductionType, T,   8>( out, partial, in, N, numBlocks );
+        case   16: return Reduction5_template<ReductionType, T,  16>( out, partial, in, N, numBlocks );
+        case   32: return Reduction5_template<ReductionType, T,  32>( out, partial, in, N, numBlocks );
+        case   64: return Reduction5_template<ReductionType, T,  64>( out, partial, in, N, numBlocks );
+        case  128: return Reduction5_template<ReductionType, T, 128>( out, partial, in, N, numBlocks );
+        case  256: return Reduction5_template<ReductionType, T, 256>( out, partial, in, N, numBlocks );
+        case  512: return Reduction5_template<ReductionType, T, 512>( out, partial, in, N, numBlocks );
+        case 1024: return Reduction5_template<ReductionType, T,1024>( out, partial, in, N, numBlocks );
     }
 }
