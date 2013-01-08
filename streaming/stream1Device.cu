f96a5d4271222f0336a5b14c650844830670a1b4
Nicholas Wilt <nicholas@archaeasoftware.com>
2013-01-08 11:32:05 -0800

    Wall clock timing, and do not use pinned host memory

--- streaming/stream1Device.cu
+++ streaming/stream1Device.cu
@@ -42,6 +42,7 @@
 
 #include <chError.h>
 #include <chCommandLine.h>
+#include <chTimer.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -105,6 +106,7 @@ saxpyGPU( float *out, const float *px, const float *py, size_t N, float alpha )
 cudaError_t
 MeasureTimes( 
     float *msTotal,
+    float *msWallClock,
     float *msHtoD, 
     float *msKernel, 
     float *msDtoH, 
@@ -114,6 +116,7 @@ MeasureTimes(
     int nThreads )
 {
     cudaError_t status;
+    chTimerTimestamp chStart, chStop;
     float *dptrOut = 0, *hptrOut = 0;
     float *dptrY = 0, *hptrY = 0;
     float *dptrX = 0, *hptrX = 0;
@@ -122,10 +125,10 @@ MeasureTimes(
     cudaEvent_t evKernel = 0;
     cudaEvent_t evDtoH = 0;
 
-    CUDART_CHECK( cudaHostAlloc( &hptrOut, N*sizeof(float), 0 ) );
+    hptrOut = new float[N];
     memset( hptrOut, 0, N*sizeof(float) );
-    CUDART_CHECK( cudaHostAlloc( &hptrY, N*sizeof(float), 0 ) );
-    CUDART_CHECK( cudaHostAlloc( &hptrX, N*sizeof(float), 0 ) );
+    hptrY = new float[N];
+    hptrX = new float[N];
 
     CUDART_CHECK( cudaMalloc( &dptrOut, N*sizeof(float) ) );
     CUDART_CHECK( cudaMemset( dptrOut, 0, N*sizeof(float) ) );
@@ -145,14 +148,27 @@ MeasureTimes(
         hptrY[i] = (float) rand() / RAND_MAX;
     }
     CUDART_CHECK( cudaEventRecord( evStart, 0 ) );
-    CUDART_CHECK( cudaMemcpyAsync( dptrX, hptrX, N*sizeof(float), cudaMemcpyHostToDevice, NULL ) );
-    CUDART_CHECK( cudaMemcpyAsync( dptrY, hptrY, N*sizeof(float), cudaMemcpyHostToDevice, NULL ) );
+
+    //
+    // begin timing
+    //
+
+    chTimerGetTime( &chStart );
+    CUDART_CHECK( cudaMemcpy( dptrX, hptrX, N*sizeof(float), cudaMemcpyHostToDevice ) );
+    CUDART_CHECK( cudaMemcpy( dptrY, hptrY, N*sizeof(float), cudaMemcpyHostToDevice ) );
     CUDART_CHECK( cudaEventRecord( evHtoD, 0 ) );
         saxpyGPU<<<nBlocks, nThreads>>>( dptrOut, dptrX, dptrY, N, alpha );
     CUDART_CHECK( cudaEventRecord( evKernel, 0 ) );
-    CUDART_CHECK( cudaMemcpyAsync( hptrOut, dptrOut, N*sizeof(float), cudaMemcpyDeviceToHost, NULL ) );
+    CUDART_CHECK( cudaMemcpy( hptrOut, dptrOut, N*sizeof(float), cudaMemcpyDeviceToHost ) );
     CUDART_CHECK( cudaEventRecord( evDtoH, 0 ) );
     CUDART_CHECK( cudaDeviceSynchronize() );
+
+    //
+    // end timing
+    //
+
+    chTimerGetTime( &chStop );
+    *msWallClock = 1000.0f*chTimerElapsedTime( &chStart, &chStop );
     for ( size_t i = 0; i < N; i++ ) {
         if ( fabsf( hptrOut[i] - (alpha*hptrX[i]+hptrY[i]) ) > 1e-5f ) {
             status = cudaErrorUnknown;
@@ -171,8 +187,9 @@ Error:
     cudaFree( dptrOut );
     cudaFree( dptrX );
     cudaFree( dptrY );
-    cudaFreeHost( hptrX );
-    cudaFreeHost( hptrY );
+    delete[] hptrOut;
+    delete[] hptrX;
+    delete[] hptrY;
     return status;
 }
 
@@ -208,12 +225,12 @@ main( int argc, char *argv[] )
     CUDART_CHECK( cudaSetDevice( device ) );
     CUDART_CHECK( cudaSetDeviceFlags( cudaDeviceMapHost ) );
     {
-        float msTotal, msHtoD, msKernel, msDtoH;
-        CUDART_CHECK( MeasureTimes( &msTotal, &msHtoD, &msKernel, &msDtoH, N, alpha, nBlocks, nThreads ) );
-        printf( "Total time: %.2f ms (%.2f MB/s)\n", msTotal, Bandwidth( msTotal, 3*N*sizeof(float) ) );
+        float msTotal, msWallClock, msHtoD, msKernel, msDtoH;
+        CUDART_CHECK( MeasureTimes( &msTotal, &msWallClock, &msHtoD, &msKernel, &msDtoH, N, alpha, nBlocks, nThreads ) );
         printf( "Memcpy( host->device ): %.2f ms (%.2f MB/s)\n", msHtoD, Bandwidth( msHtoD, 2*N*sizeof(float) ) );
         printf( "Kernel processing     : %.2f ms (%.2f MB/s)\n", msKernel, Bandwidth( msKernel, 3*N*sizeof(float) ) );
-        printf( "Memcpy (device->host ): %.2f ms (%.2f MB/s)\n", msDtoH, Bandwidth( msDtoH, N*sizeof(float) ) );
+        printf( "Memcpy (device->host ): %.2f ms (%.2f MB/s)\n\n", msDtoH, Bandwidth( msDtoH, N*sizeof(float) ) );
+        printf( "Total time (wall clock): %.2f ms (%.2f MB/s)\n", msWallClock, Bandwidth( msWallClock, 3*N*sizeof(float) ) );
     }
 
 Error:
