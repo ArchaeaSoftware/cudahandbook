3f4f85c220d3fb5299e74f994eba6a79d548a75b
Nicholas Wilt <nwilt@archaeasoftware.com>
2013-07-26 23:57:28 +0000

    Do a reduction across each row to compute value to fire into atomic

--- histogram/histogramSharedPrivatized32.cuh
+++ histogram/histogramSharedPrivatized32.cuh
@@ -73,14 +73,13 @@ histogram1DSharedPrivatized32(
     const unsigned char *base, size_t N )
 {
     extern __shared__ unsigned int privHist[];
-//    unsigned int *myHist = privHist+64*threadIdx.x;
     for ( int i = threadIdx.x;
               i < 64*blockDim.x;
               i += blockDim.x ) {
         privHist[i] = 0;
     }
     __syncthreads();
-#define CACHE_IN_REGISTER 0
+#define CACHE_IN_REGISTER 1
 #if CACHE_IN_REGISTER
     int cacheIndex = 0;
     unsigned int cacheValue = 0;
@@ -93,19 +92,65 @@ histogram1DSharedPrivatized32(
         int index = pixval>>2;
 #if CACHE_IN_REGISTER
         if ( index != cacheIndex ) {
-            privHist[64*threadIdx.x+cacheIndex] = cacheValue;
+            privHist[cacheIndex*blockDim.x+threadIdx.x] = cacheValue;
             cacheIndex = index;
-            cacheValue = privHist[64*threadIdx.x+index];
+            cacheValue = privHist[index*blockDim.x+threadIdx.x];
         }
         cacheValue += increment;
 #else
-        privHist[64*threadIdx.x+index] += increment;
+        privHist[index*blockDim.x+threadIdx.x] += increment;
 #endif
     }
 #if CACHE_IN_REGISTER
-    privHist[64*threadIdx.x+cacheIndex] = cacheValue;
+    privHist[cacheIndex*blockDim.x+threadIdx.x] = cacheValue;
 #endif
     __syncthreads();
+#if 1
+    for ( int i = 0; i < 64; i++ ) {
+        unsigned int sum;
+        volatile unsigned int *histBase = &privHist[i*64+threadIdx.x];
+        unsigned int myValue = histBase[0];
+        privHist[i*64+threadIdx.x] = myValue & 0xff00ff;
+        __syncthreads();
+        myValue >>= 8;
+        if ( threadIdx.x < 32 ) {
+            histBase[0] += histBase[32];
+            histBase[0] += histBase[16];
+            histBase[0] += histBase[ 8];
+            histBase[0] += histBase[ 4];
+            histBase[0] += histBase[ 2];
+            sum = histBase[0] + histBase[1];
+            if ( threadIdx.x==0 && (sum&0xffff) ) atomicAdd( &pHist[i*4+0], sum&0xffff );
+            sum >>= 16;
+            if ( threadIdx.x==0 && sum ) atomicAdd( &pHist[i*4+2], sum );
+        }
+
+        histBase[0] = myValue & 0xff00ff;
+        __syncthreads();
+        if ( threadIdx.x < 32 ) {
+            histBase[0] += histBase[32];
+            histBase[0] += histBase[16];
+            histBase[0] += histBase[ 8];
+            histBase[0] += histBase[ 4];
+            histBase[0] += histBase[ 2];
+            sum = histBase[0] + histBase[1];
+            if ( threadIdx.x==0 && (sum&0xffff) ) atomicAdd( &pHist[i*4+1], sum&0xffff );
+            sum >>= 16;
+            if ( threadIdx.x==0 && sum ) atomicAdd( &pHist[i*4+3], sum );
+        }
+        
+    }
+#if 0
+    for ( int i = 0; i < 64; i++ ) {
+        unsigned int count = privHist[i*64/*blockDim.x*/+threadIdx.x];
+        atomicAdd( &pHist[i*4+0], count & 0xff );  count >>= 8;
+        atomicAdd( &pHist[i*4+1], count & 0xff );  count >>= 8;
+        atomicAdd( &pHist[i*4+2], count & 0xff );  count >>= 8;
+        atomicAdd( &pHist[i*4+3], count );
+    }
+#endif
+#else
+
     for ( int i = threadIdx.x;
               i < 64*blockDim.x;
               i += blockDim.x ) {
@@ -116,7 +161,7 @@ histogram1DSharedPrivatized32(
         atomicAdd( &pHist[idx*4+2], count & 0xff );  count >>= 8;
         atomicAdd( &pHist[idx*4+3], count );
     }
-
+#endif
 }
 
 void
