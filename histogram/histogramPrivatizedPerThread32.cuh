4af5935af969c65cd6e1ac5dbe7b1b2ff9fccf98
Nicholas Wilt <nicholas@archaeasoftware.com>
2013-08-27 21:53:16 -0700

    Remove dead code

--- histogram/histogramPrivatizedPerThread32.cuh
+++ histogram/histogramPrivatizedPerThread32.cuh
@@ -81,62 +81,7 @@ histogram1DPrivatizedPerThread32(
     }
     __syncthreads();
 
-#if 0
-    for ( int i = 0; i < 32; i++ ) {
-        unsigned int *histBase0 = &privHist[i*64+threadIdx.x];
-        unsigned int *histBase1 = &privHist[(i+32)*64+threadIdx.x];
-        unsigned int my0 = histBase0[0];
-        unsigned int my1 = histBase1[0];
-        histBase0[0] = (my0 & 0xff00ff)+(my1 & 0xff00ff);
-        my0 >>= 8;
-        my1 >>= 8;
-        histBase1[0] = (my0 & 0xff00ff)+(my1 & 0xff00ff);
-    }
-    if ( blockIdx.x==0 ) {
-        pHist[threadIdx.x] = privHist[threadIdx.x];
-    }
-    return;
-    
-    __syncthreads();
-    
-    // now the top 32 rows have partial sums for bytes 0 and 2, and
-    // the bottom 32 rows have partial sums for bytes 1 and 3
-    //
-    // do a log-step reduction by row
-    //
-    for ( int numRows = 16; numRows >= 1; numRows >>= 1 )  {
-        for ( int i = 0; i < numRows; i++ ) {
-            unsigned int *histBase02 = &privHist[i*64+threadIdx.x];
-            histBase02[0] += histBase02[numRows*64];
-
-            unsigned int *histBase13 = &privHist[(i+32)*64+threadIdx.x];
-            histBase13[0] += histBase13[numRows*64];
-        }
-        __syncthreads();
-    }
-
-    // now row 0 has sums for bytes 0 and 2, and
-    // row 32 has sums for bytes 1 and 3.
-
-    if ( threadIdx.x < 32 ) {
-        unsigned int sum02 = privHist[threadIdx.x];
-        if ( sum02&0xffff ) atomicAdd( &pHist[threadIdx.x*4+0], sum02&0xffff );
-        sum02 >>= 16;
-        if ( sum02 ) atomicAdd( &pHist[threadIdx.x*4+2], sum02 );
-    }
-    else {
-        unsigned int sum13 = privHist[64*32+threadIdx.x];
-        if ( sum13&0xffff ) atomicAdd( &pHist[threadIdx.x*4+0], sum13&0xffff );
-        sum13 >>= 16;
-        if ( sum13 ) atomicAdd( &pHist[threadIdx.x*4+2], sum13 );
-    }
-
-#endif
-
 #if 1
-// this works and there is still room for improvement (by having threads 32-63 of the block do half the work)
-// but we should be able to do a log-step reduction instead of looping 64 times.
-
     for ( int i = 0; i < 64; i++ ) {
         unsigned int sum;
         volatile unsigned int *histBase = &privHist[i*64+threadIdx.x];
@@ -160,27 +105,6 @@ histogram1DPrivatizedPerThread32(
         sum >>= 16;
         if ( threadIdx.x==0 ) atomicAdd( &pHist[i*4+2], sum&0xffff );
         if (threadIdx.x==63) atomicAdd( &pHist[i*4+3], sum&0xffff );
-    }
-#if 0
-    for ( int i = 0; i < 64; i++ ) {
-        unsigned int count = privHist[i*64/*blockDim.x*/+threadIdx.x];
-        atomicAdd( &pHist[i*4+0], count & 0xff );  count >>= 8;
-        atomicAdd( &pHist[i*4+1], count & 0xff );  count >>= 8;
-        atomicAdd( &pHist[i*4+2], count & 0xff );  count >>= 8;
-        atomicAdd( &pHist[i*4+3], count );
-    }
-#endif
-#else
-
-    for ( int i = threadIdx.x;
-              i < 64*blockDim.x;
-              i += blockDim.x ) {
-        int idx = i & 63;
-        unsigned int count = privHist[i];
-        atomicAdd( &pHist[idx*4+0], count & 0xff );  count >>= 8;
-        atomicAdd( &pHist[idx*4+1], count & 0xff );  count >>= 8;
-        atomicAdd( &pHist[idx*4+2], count & 0xff );  count >>= 8;
-        atomicAdd( &pHist[idx*4+3], count );
     }
 #endif
 }
--- histogram/histogramPrivatizedPerThread4x32.cuh
+++ histogram/histogramPrivatizedPerThread4x32.cuh
@@ -104,58 +104,6 @@ histogram1DPrivatizedPerThread4x32(
     }
     __syncthreads();
 
-#if 0
-    for ( int i = 0; i < 32; i++ ) {
-        unsigned int *histBase0 = &privHist[i*64+threadIdx.x];
-        unsigned int *histBase1 = &privHist[(i+32)*64+threadIdx.x];
-        unsigned int my0 = histBase0[0];
-        unsigned int my1 = histBase1[0];
-        histBase0[0] = (my0 & 0xff00ff)+(my1 & 0xff00ff);
-        my0 >>= 8;
-        my1 >>= 8;
-        histBase1[0] = (my0 & 0xff00ff)+(my1 & 0xff00ff);
-    }
-    if ( blockIdx.x==0 ) {
-        pHist[threadIdx.x] = privHist[threadIdx.x];
-    }
-    return;
-    
-    __syncthreads();
-    
-    // now the top 32 rows have partial sums for bytes 0 and 2, and
-    // the bottom 32 rows have partial sums for bytes 1 and 3
-    //
-    // do a log-step reduction by row
-    //
-    for ( int numRows = 16; numRows >= 1; numRows >>= 1 )  {
-        for ( int i = 0; i < numRows; i++ ) {
-            unsigned int *histBase02 = &privHist[i*64+threadIdx.x];
-            histBase02[0] += histBase02[numRows*64];
-
-            unsigned int *histBase13 = &privHist[(i+32)*64+threadIdx.x];
-            histBase13[0] += histBase13[numRows*64];
-        }
-        __syncthreads();
-    }
-
-    // now row 0 has sums for bytes 0 and 2, and
-    // row 32 has sums for bytes 1 and 3.
-
-    if ( threadIdx.x < 32 ) {
-        unsigned int sum02 = privHist[threadIdx.x];
-        if ( sum02&0xffff ) atomicAdd( &pHist[threadIdx.x*4+0], sum02&0xffff );
-        sum02 >>= 16;
-        if ( sum02 ) atomicAdd( &pHist[threadIdx.x*4+2], sum02 );
-    }
-    else {
-        unsigned int sum13 = privHist[64*32+threadIdx.x];
-        if ( sum13&0xffff ) atomicAdd( &pHist[threadIdx.x*4+0], sum13&0xffff );
-        sum13 >>= 16;
-        if ( sum13 ) atomicAdd( &pHist[threadIdx.x*4+2], sum13 );
-    }
-
-#endif
-
 #if 1
 
     unsigned int sum02 = 0;
@@ -174,11 +122,10 @@ histogram1DPrivatizedPerThread4x32(
     atomicAdd( &pHist[threadIdx.x*4+1], sum13&0xffff );
     sum13 >>= 16;
     atomicAdd( &pHist[threadIdx.x*4+3], sum13 );
-    
-#if 0
-// this works and there is still room for improvement (by having threads 32-63 of the block do half the work)
-// but we should be able to do a log-step reduction instead of looping 64 times.
 
+#endif
+
+#if 0
     for ( int i = 0; i < 64; i++ ) {
         unsigned int sum;
         volatile unsigned int *histBase = &privHist[i*64+threadIdx.x];
@@ -214,7 +161,6 @@ histogram1DPrivatizedPerThread4x32(
         volatile unsigned int *histBase = &privHist[i*64+threadIdx.x];
         unsigned int myValue = histBase[0];
         int sum02, sum13;
-
         sum02 = myValue & 0xff00ff;
         myValue >>= 8;
         sum13 = myValue & 0xff00ff;
@@ -242,29 +188,6 @@ histogram1DPrivatizedPerThread4x32(
             sum13 >>= 16;
             if ( sum13 ) atomicAdd( &pHist[i*4+3], sum13 );
         }
-    }
-#endif
-    
-#if 0
-    for ( int i = 0; i < 64; i++ ) {
-        unsigned int count = privHist[i*64/*blockDim.x*/+threadIdx.x];
-        atomicAdd( &pHist[i*4+0], count & 0xff );  count >>= 8;
-        atomicAdd( &pHist[i*4+1], count & 0xff );  count >>= 8;
-        atomicAdd( &pHist[i*4+2], count & 0xff );  count >>= 8;
-        atomicAdd( &pHist[i*4+3], count );
-    }
-#endif
-#else
-
-    for ( int i = threadIdx.x;
-              i < 64*blockDim.x;
-              i += blockDim.x ) {
-        int idx = i & 63;
-        unsigned int count = privHist[i];
-        atomicAdd( &pHist[idx*4+0], count & 0xff );  count >>= 8;
-        atomicAdd( &pHist[idx*4+1], count & 0xff );  count >>= 8;
-        atomicAdd( &pHist[idx*4+2], count & 0xff );  count >>= 8;
-        atomicAdd( &pHist[idx*4+3], count );
     }
 #endif
 }
