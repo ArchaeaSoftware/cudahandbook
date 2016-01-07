75659d475439bada20ba616a9aa46b8fc319bb3d
EC2 Default User <ec2-user@ip-10-197-144-145.us-west-2.compute.internal>
2016-01-07 21:01:42 +0000

    Add C++11 formulation of SAXPY

--- streaming/saxpyGPU.cuh
+++ streaming/saxpyGPU.cuh
@@ -43,6 +43,32 @@
 // Due to low arithmetic density, this kernel is extremely bandwidth-bound.
 //
 
+#if CUDAHANDBOOK_USE_CPP11
+
+#include <range.hpp>
+
+using namespace util::lang;
+using util::lang::range;
+using util::lang::indices;
+
+template<typename T>
+using step_range = typename range_proxy<T>::step_range_proxy;
+
+template<typename T>
+__device__
+step_range<T> grid_stride_range(T begin, T end) {
+    begin += blockDim.x * blockIdx.x + threadIdx.x;
+    return range(begin,end).step(gridDim.x*blockDim.x);
+}
+
+__global__ void
+saxpyGPU(  float *out, const float *x, const float *y, size_t N, float alpha  )
+{
+    for ( auto i : grid_stride_range((size_t) 0,N) ) {
+        out[i] = alpha*x[i]+y[i];
+    }
+}
+#else
 __global__ void
 saxpyGPU( float *out, const float *x, const float *y, size_t N, float alpha )
 {
@@ -52,5 +78,7 @@ saxpyGPU( float *out, const float *x, const float *y, size_t N, float alpha )
         out[i] = alpha*x[i]+y[i];
     }
 }
+
+#endif
 
 #endif
